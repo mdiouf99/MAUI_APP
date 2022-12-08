6943d73215e029f018b017f28ef113af8281727a
mdioufpro99@gmail.com <mdioufpro99@gmail.com>
2022-12-08 22:04:35 +0000

    first commit

---
+++ mslearn-dotnetmaui-create-multi-page-apps-main/exercise3/start/Astronomy/Astronomy/Pages/AstronomicalBodiesPage.xaml.cs
@@ -0,0 +1,19 @@
+namespace Astronomy.Pages;
+
+public partial class AstronomicalBodiesPage : ContentPage
+{
+	public AstronomicalBodiesPage()
+	{
+		InitializeComponent();
+        btnComet.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails");
+        btnEarth.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails");
+        btnMoon.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails");
+        btnSun.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails");
+
+        btnComet.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=comet");
+        btnEarth.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=earth");
+        btnMoon.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=moon");
+        btnSun.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=sun");
+
+    }
+}
